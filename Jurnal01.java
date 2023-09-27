fcccc4ff7e138c009dceedd1fc9e18fbed86b0b4
ARYA ERSI PUTRA <138345958+ZGE26@users.noreply.github.com>
2023-09-27 09:26:05 +0700

    Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,10 @@
+package part6;
+
+public class Jurnal01 {
+    public static void main(String[] args) {
+       
+        System.out.println("Nama : ARYA ERSI PUTRA");
+        System.out.println("Nim : 607062300120");
+        System.out.println("Kelas : D3IF - 47 - 04");
+    }
+}
